80bf6c5a9df633e1d0072b68899c0b84549c05fb
Isaac Cabello <cabelloisaac97@gmaill.com>
2021-01-22 01:03:38 -0500

    Removed FavoritesViewModel

--- app/src/main/java/com/example/postsisaac/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/postsisaac/ui/favorites/FavoritesFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
@@ -22,7 +21,6 @@ import kotlinx.coroutines.launch
 
 class FavoritesFragment : Fragment() {
 
-    private lateinit var favoritesViewModel: FavoritesViewModel
     private var posts: ArrayList<Post> = ArrayList()
     private lateinit var postsAdapter: PostsAdapter
     private lateinit var db: PostsDb
@@ -35,9 +33,6 @@ class FavoritesFragment : Fragment() {
 
         val root = inflater.inflate(R.layout.fragment_favorites, container, false)
         val recyclerView: RecyclerView = root.findViewById(R.id.recycler_view)
-
-        favoritesViewModel =
-            ViewModelProvider(this).get(FavoritesViewModel::class.java)
 
         val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
 
